563e34b4e4aa4ad2e8475d98edb48051cd01ce08
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2024-05-14 20:11:17 +0200

    crea modulos y rutas básicas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // lazy load de lasrutas del modulo authmodule
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    // lazy load de lasrutas del modulo heroesmodule
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule),
+  },
+  // path al componente de error404
+  {
+    path: '404',
+    component: Error404PageComponent,
+  },
+  // path por defecto, redirige a heroes
+  {
+    path: '',
+    redirectTo: 'heroes',
+    // Para garantizar que la redireecion solo se produce cuando el path es exactamente el indicado
+    pathMatch: 'full'
+  },
+  // Cualquier otro path redirige a la ruta/componente error404
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
+
+// localhost:4200/heroes/
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: 'new-hero',
+        component: NewHeroPageComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+})
+export class HeroesRoutingModule { }
