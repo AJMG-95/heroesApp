282c9617d9582a5e1bc55b25f0f2bcf1f640e299
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2024-05-15 23:18:29 +0200

    Implementa angularMaterial y primeFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -10,7 +12,9 @@ import { SharedModule } from './shared/shared.module';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+
     AppRoutingModule,
     SharedModule,
   ],
